978036c4728ff8f5d33000d71602e25c7e6cfd77
DanceParty <keevandance@gmail.com>
2021-10-07 00:47:02 -0700

    add some consistent styling and change background color

--- components/main.js
+++ components/main.js
@@ -5,12 +5,13 @@ import Navigation from "./navigation"
 export default function Main({ data, children }) {
   return (
     <main>
-      <h1 className={styles["title"]}>
-        <Link href="/">
-          <a className={styles["text-decoration-none"]}>danceparty</a>
-        </Link>
-      </h1>
-      <hr />
+      <div className={styles["title-wrapper"]}>
+        <h1 className={styles["title"]}>
+          <Link href="/">
+            <a className={styles["text-decoration-none"]}>danceparty</a>
+          </Link>
+        </h1>
+      </div>
       <div className={[styles["flex"], styles["flex-row"]].join(" ")}>
         <Navigation data={data} />
 
